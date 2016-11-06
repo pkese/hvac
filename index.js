65e74c2c89517b9b216db7432d4672437b926a63
Peter Kese <peter.kese@viidea.com>
2016-11-06 19:42:17 +0100

    Replace Express with Feathers

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 "use strict"
 
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var feathers = require('feathers');
+var socketio = require('feathers-socketio')
+
 var state = require('./state');
 
 var controller = require('./controller')
 
 //Basic-Auth config
 var auth = require('basic-auth');
+
+var app = feathers()
 
 /*
 credentials file should contain:
@@ -68,39 +70,45 @@ app.get('/', function(req, res){
   }
 });
 
-io.on('connection', function (socket) {
-  if (data) socket.emit('data', data);
-  let setHandler = propname => {
-    socket.on(propname, value => {
-      console.log("socket.io:", propname, "=", value);
-      state[propname] = value;
+app.configure(socketio(function(io) {
+  io.on('connection', function (socket) {
+    if (data) socket.emit('data', data);
+    let setHandler = propname => {
+      socket.on(propname, value => {
+        console.log("socket.io:", propname, "=", value);
+        state[propname] = value;
+        controller.decide();
+        controller.refresh();
+      });
+    }
+    ['L0active','L1active','L0target_temp','L1target_temp'].forEach(setHandler);
+
+    socket.on('rf-temp', function(data) {
+      //console.log('got rf:', data)
+      if (!data.channel || typeof data.temp != 'number')
+        return
+      var target = [state.L1report,state.L0report,state.L2report][data.channel-1];
+      if (typeof data.updated == 'string') data.updated = Date.parse(data.updated);
+      // Object.assign(target, data);
+      target.extend(data);
+      //console.log('parsed as', data, 'into', target);
       controller.decide();
-      controller.refresh();
+      //controller.refresh();
     });
-  }
-  ['L0active','L1active','L0target_temp','L1target_temp'].forEach(setHandler);
-
-  socket.on('rf-temp', function(data) {
-    //console.log('got rf:', data)
-    if (!data.channel || typeof data.temp != 'number')
-      return
-    var target = [state.L1report,state.L0report,state.L2report][data.channel-1];
-    if (typeof data.updated == 'string') data.updated = Date.parse(data.updated);
-    // Object.assign(target, data);
-    target.extend(data);
-    //console.log('parsed as', data, 'into', target);
-    controller.decide();
-    //controller.refresh();
   });
-});
+  
+  // dispatch data update to socketio listeners...
+  function onControllerUpdate(_data) {
+    data = _data;
+    io.emit('data', data)
+  }
 
-http.listen(80, function(){
+  // initialize the controller
+  controller(onControllerUpdate)
+
+}))
+
+app.listen(80, function(){
   console.log('listening on *:80');
 });
 
-function onControllerUpdate(_data) {
-  data = _data;
-  io.emit('data', data)
-}
-
-controller(onControllerUpdate)
